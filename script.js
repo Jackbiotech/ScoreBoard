a2826ddedddbd9a4af3a2db3c30ad5c8e906816d
Jackbiotech <108440450+Jackbiotech@users.noreply.github.com>
2022-12-13 11:10:31 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+`use strict`
+
+let homeDigits = document.getElementById(`home-digits`)
+
+function increase1() {
+    homeDigits.textContent++;
+}
+
+function increase2() {
+    homeDigits.textContent++;
+    homeDigits.textContent++;
+}
+
+function increase3() {
+    homeDigits.textContent++;
+    homeDigits.textContent++;
+    homeDigits.textContent++;
+}
+
+
+let guestDigits = document.getElementById(`guest-digits`)
+
+function increase1g() {
+    guestDigits.textContent++;
+}
+
+function increase2g() {
+    guestDigits.textContent++;
+    guestDigits.textContent++;
+}
+
+function increase3g() {
+    guestDigits.textContent++;
+    guestDigits.textContent++;
+    guestDigits.textContent++;
+}
